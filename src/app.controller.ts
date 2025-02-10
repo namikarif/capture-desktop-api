b0f3a07a3a1c133e6154ea3d8201f9c6e574b4ee
Namig <45629136+namikarif@users.noreply.github.com>
2025-02-10 13:20:17 +0400

    Update and rename src/app.controller.ts to src/controllers/app.controller.ts

--- src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('computers')
-export class AppController {
-  @Get()
-  getComputers() {
-    return { computers: ['Computer1', 'Computer2', 'Computer3'] };
-  }
-}
