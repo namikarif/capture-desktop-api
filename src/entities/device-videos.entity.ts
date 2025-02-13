198eeb2fcde3645bedf7310da695a36d8e96502d
Namig <45629136+namikarif@users.noreply.github.com>
2025-02-13 15:05:27 +0400

    Update device-videos.entity.ts

--- src/entities/device-videos.entity.ts
+++ src/entities/device-videos.entity.ts
@@ -6,7 +6,40 @@ import {
   ObjectId,
   ObjectIdColumn,
   UpdateDateColumn,
+} from 'typeorm';import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ObjectId,
+  ObjectIdColumn,
+  UpdateDateColumn,
 } from 'typeorm';
+
+@Entity('device_videos')
+export class DeviceVideosEntity {
+  @ObjectIdColumn()
+  _id: ObjectId;
+
+  @Column()
+  deviceId: string;
+
+  @Column()
+  videoPath: string;
+
+  @Column()
+  videoTime: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+}
+
 
 @Entity('device_videos')
 export class DeviceVideosEntity {
