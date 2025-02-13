75b66bed3e44e12cb705cc251f20119828934d30
Namig <45629136+namikarif@users.noreply.github.com>
2025-02-13 13:52:00 +0400

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './controllers/app.controller';
-import { AppService } from './services/app.service';
-import { AppGateway } from './services/app.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DeviceController } from './controllers/device.controller';
+import { DeviceService } from './services/device.service';
+import { SocketGateway } from './services/socket.gateway';
+import { DeviceEntity } from './entities/device.entity';
+import { DeviceVideosEntity } from './entities/device-videos.entity';
+
+const mongoPass = 'bsw1cXhwpdnSEAeR';
+const mongoUri = `mongodb+srv://namigabdukarimov:${mongoPass}@capture-desktop.ducum.mongodb.net/db?retryWrites=true&w=majority&appName=capture-desktop`;
+
+const entities = [DeviceEntity, DeviceVideosEntity];
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, AppGateway],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: mongoUri,
+      database: 'db',
+      logger: 'advanced-console',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      synchronize: false,
+      autoLoadEntities: true,
+    }),
+    TypeOrmModule.forFeature(entities),
+  ],
+  controllers: [DeviceController],
+  providers: [DeviceService, SocketGateway],
 })
 export class AppModule {}
